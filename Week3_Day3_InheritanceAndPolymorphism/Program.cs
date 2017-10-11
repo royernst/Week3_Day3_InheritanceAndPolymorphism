be3fd22aeca7069fa68a99c74870e38bab2f547e
royernst <roy.e31@hotmail.com>
2017-10-11 10:01:58 -0400

    Initial commit

---
+++ Week3_Day3_InheritanceAndPolymorphism/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day3_InheritanceAndPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
